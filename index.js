964d6e461bb33266aaebfeec184f68b5087ef84d
Simon <simon@jampot.design>
2016-09-20 00:03:43 +0000

    starting inital project, done the b u i l d and set up basic page as per course

---
+++ index.js
@@ -0,0 +1,33 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Person = function() {
+    var name = 'Derek Zoolander';
+    var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
+    var job = 'Male model';
+    return (
+        <div className="person">
+            <div className="person-name">{name}</div>
+            <img className="person-img" src={imageUrl} />
+            <div className="person-job">
+                {job}
+            </div>
+        </div>
+    );
+};
+
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+        </div>
+    );
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
+});
